79f00555001ac9f128605ad86c53f939fd09e6f1
Mrunali ukey <ukey.k002@gmail.com>
2023-06-16 02:34:33 +0530

    minor change

--- 2.4 QR Code Project/solution.js
+++ 2.4 QR Code Project/solution.js
@@ -16,7 +16,7 @@ inquirer
 
     fs.writeFile("URL.txt", url, (err) => {
       if (err) throw err;
-      console.log("The file has been saved!");
+      console.log("The file has been savedd!");
     });
   })
   .catch((error) => {
